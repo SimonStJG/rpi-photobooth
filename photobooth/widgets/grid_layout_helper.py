c8b78e07fce7e34d18900f79de1393bed903c989
SimonStJG <Simon.StJG@gmail.com>
2020-04-18 21:54:23 +0100

    Tidy up the printing page

--- photobooth/widgets/grid_layout_helper.py
+++ photobooth/widgets/grid_layout_helper.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QGridLayout
 GRID_LAYOUT_CONTENT_MARGIN = 100
 
 
-# TODO Work out how to set this with qss
 def set_grid_content_margins(grid_layout: QGridLayout):
+    # This is not possible via qss https://bugreports.qt.io/browse/QTBUG-22862 so this
+    #   bodge will have to do.
     grid_layout.setContentsMargins(*([GRID_LAYOUT_CONTENT_MARGIN] * 4))
--- photobooth/widgets/printing_widget.py
+++ photobooth/widgets/printing_widget.py
@@ -1,8 +1,9 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QLabel, QWidget
+from PyQt5.QtWidgets import QLabel, QWidget, QGridLayout
 
 from photobooth.uic import loadUi
+from photobooth.widgets.grid_layout_helper import set_grid_content_margins
 
 
 class PrintingWidget(QWidget):
@@ -10,6 +11,7 @@ class PrintingWidget(QWidget):
         super().__init__(parent, flags)
         loadUi("printing.ui", self)
         self._printingImage: QLabel = self.findChild(QLabel, "printingImage")
+        set_grid_content_margins(self.findChild(QGridLayout, "gridLayout"))
 
     def set_image(self, image: QImage):
         self._printingImage.setPixmap(QPixmap().fromImage(image, Qt.AutoColor))
